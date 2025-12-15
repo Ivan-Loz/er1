5034a9c7196613ad941db6923df5c8eee8f0f639
Ivan-Loz <vebikt@gmail.com>
2025-12-15 19:35:41 +0300

    Add files via upload

---
+++ ddd/cons.js
@@ -0,0 +1,60 @@
+// console.log('Привет')
+// const title = document.querySelector('h1')
+// const btn = document.querySelector('button')
+
+// btn.addEventListener('click' , () => {
+//     title.textContent = 'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG'
+//     title.style.color = 'red'
+// })
+
+
+// let userName = "Ivan"
+// console.log(typeof userName, userName)
+// userName = 222
+// console.log(typeof userName, userName)
+// userName = true
+// console.log(typeof userName, userName)
+// userName  
+// console.log(typeof userName, userName)
+// userName = null
+// console.log(typeof userName, userName)
+// userName //symbol
+// // console.log(typeof userName, userName)
+
+// let userLogin = prompt('Введите ваше имя')
+// alert(userLogin)
+
+// let next = confirm('Дашь денег?')
+// console.log(typeof next,next)
+
+
+
+// let number0 = 3.5222233
+
+// console.log(Math.floor(number0))
+// console.log(Math.ceil(number0))
+// console.log(Math.round(number0))
+
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+// console.log(Math.random())
+
+
+// let userNum = prompt("Угодай угол от 0 до 10"),
+//     copNum = Math.round(Math.random() *10)
+
+// console.log("user =" ,userNum, 'comp =', copNum)
+// max = 13
+// min = 6
+// let maRand = Math.round(Math.random() *(max-min + 1)) + min
+// console.log(maRand)
+
+// let number = 56
+// console.log(Math.max( 22,34,88,number,33,52,63))
+
+
+let nub0 = prompt('Введите длину квадрата')
+console.log(nub0 * nub0)
