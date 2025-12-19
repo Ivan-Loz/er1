8a5e2f8bef23f319f893f6b8633f924905d9c73a
vebikt <vebikt@gmail.com>
2025-12-19 20:53:24 +0300

    JavaS

---
+++ J-j3.1/main.js
@@ -0,0 +1,66 @@
+// let numb = 10, numb1 = 1000;
+// function sym(num1 = 0,num2 = 222){
+//     return num1+num2;
+// }
+// let price = sym(1000,200)
+// console.log(price)
+
+//=================================================================
+// function print(name){
+//     console.log("Кью кью!",name)
+// }
+// print("Иван")
+
+//=================================================================
+
+// let numb = 1000, numb1 = 222;
+// function sum(numb,num1,){
+//     console.log("numb =",num)
+//     console.log("numb1 =",num1)
+//     console.log(numb + num1)
+// }
+// sum(1,2)
+// console.log(numb)
+
+//=================================================
+
+// function summa(...rest){
+//     let sum = 0
+//     for (let i = 0;i < rest.length;i++){
+//         sum = rest[i] + sum
+//     }
+//     console.log(sum)
+// }
+// summa(2,4,5)
+// summa(4,2,100)
+
+//=====================================
+// function print(){
+//     console.log("Привет там кто-то кью кью")
+// }
+
+// setTimeout(print,3000)
+// print();
+// console("ДААААА...")
+
+//======================================================
+// const s = function (a,b){
+//     console.log(a +b)
+// }
+
+// const s1 = (a,b) => console.log(a+b) Функционалные выражения
+
+//--------------------------------------------------------------- Задача
+
+// function ran(num0,num1){
+//     if (num0 < num1){
+//         return num0
+//     }
+//     else if(num1 < num0){
+//         return num1
+//     }
+//     else{
+//         return num0
+//     }
+// }
+// console.log(ran(2,5))
