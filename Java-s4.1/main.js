2dad69ab70c0e88ebc0624f3b4bc93a87c7a7a98
vebikt <vebikt@gmail.com>
2025-12-26 20:46:02 +0300

    Java-S
    хочу скалу

---
+++ Java-s4.1/main.js
@@ -0,0 +1,74 @@
+// const arr = [[1,2,33,31],["а","вф","ввв","пр","кк","аапф"]]
+// console.log(arr[1][0])
+
+
+// const users = [
+//     {name: "Иван",email: "муретка@торелка.ru",password: "vebit1221"},       
+//     {name: "Олег",email: "муретка1@торелка.ru",password: "der1111"},       
+//     {name: "Брат",email: "муретка2@торелка.ru",password: "dudu7765"},
+//     {name: "Иван",email: "муретка3@торелка.ru",password: "vebit1221"},       
+// ]
+
+// user = prompt("Имя")
+
+// const res = users.find((item) => {
+//     // x = item.name
+//     // if (x === "Брат") {
+//     //     console.log("Наш бро")
+//     //     return
+//     // }
+//     // if(index === 2) {
+//     //     console.log("Наш бро")
+//     //     return
+//     // }
+//     if (item.name === user){
+//         return item
+//     }
+// })
+// console.log(res)
+
+// const res = users.filter((item)=> item.name === user)
+// console.log(res)
+
+// res ? console.log(res) : console.log("Ошибка")
+
+// users.forEach((item) => {
+//     item.password = "test"
+// })
+
+// const newUs = users.map((item) => {
+//     return {id: item.name , phone: "Нету"}
+// })
+// console.log(newUs)
+
+// const names = ["Иван","иван","Анна","Андрей","Вася","виктор"]
+
+// const newarr =names.toSorted()
+// console.log(names)
+// console.log(newarr)
+
+// const myNum = [1,34,66,23,55,2,5,4,3]
+
+// function comp(a,b){
+//     if(a > b) return 1
+//     else if(a < b) return -1
+//     else if(a === b) return 0
+
+// }
+
+// console.log(myNum.sort(comp))
+
+// const names = ["Иван","иван","Анна","Андрей","Вася","виктор"]
+
+// let strN = names.join("-")
+// console.log(strN)
+
+// let ser = "Hello world"
+
+// const HW = ser.split(" ")
+// console.log(HW)
+
+// const myNum = [1,34,66,23,55,2,5,4,3]
+
+// let res = myNum.reduce((sum,item) => sum + item)
+// console.log(res)
---
+++ Java-s4/main.js
@@ -0,0 +1,44 @@
+// const myArr = new Array()
+// console.log((myArr));
+
+// const myArr1 = []
+
+// myArr1.unshift(1,"2",true)
+
+
+// const languages = ["С++","C#","Python","1С","Java","C#"]
+
+// const newLang = languages.with(3,"PHP")
+// console.log(languages)
+// // console.log(newLang)
+
+// languages.splice(2,1)
+
+// console.log(languages)
+
+// const new2 = languages.toSpliced("ДА",100,"аываыв",2)
+
+// const langC = languages.slice(0,2)
+
+// console.log(languages.includes("1С"))
+
+// console.log(languages.indexOf("C#",3))
+
+
+
+
+// let randNumber = Math.floor( Math.random() *10 + 1)
+
+// const ArrNum = [5,7,3,1,2]
+
+// for (let i = 0;i <= ArrNum.length;i++){
+//     if (ArrNum[i] === randNumber){
+//         console.log(`Ага! ${randNumber} Число не нужно!`)
+//         randNumber = Math.floor( Math.random() *10 + 1)
+//     }
+//     else{
+//         console.log(randNumber)
+//         break
+//     }
+// }
+
